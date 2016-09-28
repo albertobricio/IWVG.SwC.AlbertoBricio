04cd28fdad2522b9d08af5f2b8c48965c683a4ec
user <user@DESKTOP-6GJR874>
2016-09-28 13:29:43 +0200

    test añadidos, funcionamiento correcto

---
+++ src/test/java/miw/FractionTest.java
@@ -0,0 +1,37 @@
+package miw;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FractionTest {
+
+	private Fraction f1,f2;
+	
+	@Before
+	public void before()
+	{
+		f1 = new Fraction(3,6);
+		f2 = new Fraction(6,3);
+	}
+	
+	@Test
+	public void testFractionIntInt() {
+		assertEquals(3, f1.getNumerator());
+		assertEquals(6, f1.getDenominator());
+		assertEquals(6, f2.getNumerator());
+		assertEquals(3, f2.getDenominator());
+	}
+	
+	@Test
+	public void testisPropiaFraction1() {
+		assertTrue(f1.getNumerator() < f1.getDenominator());
+	}
+	
+	@Test
+	public void testisImpropiaFraction2() {
+		assertTrue(f2.getNumerator() > f2.getDenominator());
+	}
+
+}
---
+++ src/test/java/miw/UserTest.java
@@ -0,0 +1,35 @@
+package miw;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	private User usr;
+	
+	@Before
+	public void before()
+	{
+		usr = new User(23,"Alberto","Bricio");
+	}
+	
+	@Test
+	public void testNombreCompleto() {
+		assertEquals("Alberto Bricio", usr.fullName());
+	}
+	
+	@Test
+	public void testComprobarUsuarioYNumero() {
+		assertEquals(23,usr.getNumber());
+		assertEquals("Alberto", usr.getName());
+		assertEquals("Bricio", usr.getFamilyName());
+	}
+	
+	@Test
+	public void testInicialesNombre() {
+		assertEquals("A.", usr.initials());
+	}
+
+}
